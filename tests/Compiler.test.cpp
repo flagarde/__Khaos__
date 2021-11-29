ba2a3448f4311b3a30a6c11c23324d403ead3128
flagarde <flagarde.programs@pm.me>
2021-11-29 22:33:14 +0800

    Improve test

--- tests/Compiler.test.cpp
+++ tests/Compiler.test.cpp
@@ -5,5 +5,9 @@
 
 TEST_CASE("testing the Compiler macros")
 {
+#if COMPILER_IS(MSVC)
+  std::cout<<"MSVC : "<<_MSC_FULL_VER<<" Macro "<<KHAOS_COMPILER_VERSION<<" "<<KHAOS_COMPILER_VERSION_TWEAK<<" "<<std::endl;
+#else
   std::cout<<KHAOS_COMPILER_VERSION<<" "<<KHAOS_COMPILER_VERSION_TWEAK<<" "<<std::endl;
+#endif
 }
