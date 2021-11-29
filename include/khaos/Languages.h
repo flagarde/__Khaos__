dfc18afe1c415887922f7b81efd48fdb3844d879
flagarde <flagarde.programs@pm.me>
2021-11-29 19:48:54 +0800

    Add VersionMacros.h, C.h, CXX.h

--- include/khaos/Languages.h
+++ include/khaos/Languages.h
@@ -12,24 +12,121 @@
   *
   * When clang is actually compiling CUDA code – rather than being used as a subtool of NVCC’s – it defines the __CUDA__ macro.
   */
-  #define KHAOS_LANGUAGE_cuda 1
+  #define KHAOS_LANGUAGE_Cuda 1
   #if defined(__clang__) && defined(__CUDA__)
-    #define KHAOS_LANGUAGE_clang_cuda 1
+    #define KHAOS_LANGUAGE_Clang_Cuda 1
   #endif
 #elif defined(__OBJC__)
   #if defined(__cplusplus)
-    #define KHAOS_LANGUAGE_objcpp 1
+    #define KHAOS_LANGUAGE_ObjCPP 1
   #else
-    #define KHAOS_LANGUAGE_objc 1
+    #define KHAOS_LANGUAGE_ObjC 1
   #endif
 #elif defined(__ASSEMBLER__)
-  #define KHAOS_LANGUAGE_assembler 1
+  #define KHAOS_LANGUAGE_Assembler 1
+#elif defined(__GFORTRAN__)
+  #define KHAOS_LANGUAGE_Fortran 1
 #elif defined(__cplusplus)
-  #define KHAOS_LANGUAGE_cpp 1
+  #define KHAOS_LANGUAGE_CPP 1
+  #include "khaos/CXX.h"
 #else
-  #define KHAOS_LANGUAGE_c 1
+  #define KHAOS_LANGUAGE_C 1
+  #include "khaos/C.h"
 #endif
 
+/** @hideinitializer
+ * Check the language.
+ * \param[in] language The language to test.
+ *
+ * Values :
+ * | Macro      | (other value) |
+ * | :--------: | :-----------: |
+ * | Assembler  |               |
+ * | C          |               |
+ * | Clang_Cuda | Cuda          |
+ * | Cuda       |               |
+ * | CXX        |               |
+ * | Fortran    |               |
+ * | ObjC       |               |
+ * | ObjCPP     |               |
+ *
+ * \note Can be directly used in both preprocessor and compiler expressions.
+ */
 #define KHAOS_LANGUAGE_IS(language) (KHAOS_LANGUAGE_##language)
+#if KHAOS_LANGUAGE_IS(C)
+  /** @hideinitializer
+  * Check if the C, CXX standard is greater than \b standard.
+  * \param[in] standard The language to test.
+  *
+  * Values for C
+  * | Macro      |
+  * | :--------: |
+  * | C90        |
+  * | C95        |
+  * | C99        |
+  * | C11        |
+  * | C17        |
+  *
+  * Values for CXX
+  * | Macro      |
+  * | :--------: |
+  * | CXX97      |
+  * | CXX11      |
+  * | CXX14      |
+  * | CXX17      |
+  * | CXX20      |
+  *
+  * \note Can be directly used in both preprocessor and compiler expressions.
+  */
+  #define KHAOS_LANGUAGE_IS_GREATER(standard) (KHAOS_C_STANDARD > KHAOS_STANDARD_##standard)
+  /** @hideinitializer
+  * Check if the C, CXX standard is greater or equal than \b standard.
+  * \param[in] standard The language to test.
+  *
+  * Values for C and CXX : see KHAOS_LANGUAGE_IS_GREATER
+  *
+  * \note Can be directly used in both preprocessor and compiler expressions.
+  */
+  #define KHAOS_LANGUAGE_IS_GREATER_EQUAL(standard) (KHAOS_C_STANDARD >= KHAOS_STANDARD_##standard)
+  /** @hideinitializer
+  * Check if the C, CXX standard is equal than \b standard.
+  * \param[in] standard The language to test.
+  *
+  * Values for C and CXX : see KHAOS_LANGUAGE_IS_GREATER
+  *
+  * \note Can be directly used in both preprocessor and compiler expressions.
+  */
+  #define KHAOS_LANGUAGE_IS_EQUAL(standard) (KHAOS_C_STANDARD == KHAOS_STANDARD_##standard)
+  /** @hideinitializer
+  * Check if the C, CXX standard is lower or equal than \b standard.
+  * \param[in] standard The language to test.
+  *
+  * Values for C and CXX : see KHAOS_LANGUAGE_IS_GREATER
+  *
+  * \note Can be directly used in both preprocessor and compiler expressions.
+  */
+  #define KHAOS_LANGUAGE_IS_LOWER_EQUAL(standard) (KHAOS_C_STANDARD <= KHAOS_STANDARD_##standard)
+  /** @hideinitializer
+  * Check if the C, CXX standard is lower than \b standard.
+  * \param[in] standard The language to test.
+  *
+  * Values for C and CXX : see KHAOS_LANGUAGE_IS_GREATER
+  *
+  * \note Can be directly used in both preprocessor and compiler expressions.
+  */
+  #define KHAOS_LANGUAGE_IS_LOWER(standard) (KHAOS_C_STANDARD < KHAOS_STANDARD_##standard)
+#elif KHAOS_LANGUAGE_IS(CPP)
+  #define KHAOS_LANGUAGE_IS_GREATER(standard) (KHAOS_CXX_STANDARD > KHAOS_STANDARD_##standard)
+  #define KHAOS_LANGUAGE_IS_GREATER_EQUAL(standard) (KHAOS_CXX_STANDARD >= KHAOS_STANDARD_##standard)
+  #define KHAOS_LANGUAGE_IS_EQUAL(standard) (KHAOS_CXX_STANDARD == KHAOS_STANDARD_##standard)
+  #define KHAOS_LANGUAGE_IS_LOWER_EQUAL(standard) (KHAOS_CXX_STANDARD <= KHAOS_STANDARD_##standard)
+  #define KHAOS_LANGUAGE_IS_LOWER(standard) (KHAOS_CXX_STANDARD < KHAOS_STANDARD_##standard)
+#else
+  #define KHAOS_LANGUAGE_IS_GREATER(standard) (0L)
+  #define KHAOS_LANGUAGE_IS_GREATER_EQUAL(standard) (0L)
+  #define KHAOS_LANGUAGE_IS_EQUAL(standard) (0L)
+  #define KHAOS_LANGUAGE_IS_LOWER_EQUAL(standard) (0L)
+  #define KHAOS_LANGUAGE_IS_LOWER(standard) (0L)
+#endif
 
 #endif
--- tests/LanguageCXX.test.cpp
+++ tests/LanguageCXX.test.cpp
@@ -4,9 +4,46 @@
 
 TEST_CASE("testing the Language CXX")
 {
-#if KHAOS_LANGUAGE_IS(cpp)
+#if KHAOS_LANGUAGE_IS(CPP)
   CHECK(true);
 #else
   CHECK(false);
 #endif
+
+#if KHAOS_LANGUAGE_IS_EQUAL(CXX11)
+  CHECK(true);
+#else
+  CHECK(false);
+#endif
+
+#if KHAOS_LANGUAGE_IS_GREATER_EQUAL(CXX97)
+  CHECK(true);
+#else
+  CHECK(false);
+#endif
+
+#if KHAOS_LANGUAGE_IS_LOWER_EQUAL(CXX11)
+  CHECK(true);
+#else
+  CHECK(false);
+#endif
+
+#if KHAOS_LANGUAGE_IS_LOWER(CXX17)
+  CHECK(true);
+#else
+  CHECK(false);
+#endif
+
+#if KHAOS_LANGUAGE_IS(ISO)
+  CHECK(true);
+#else
+  CHECK(false);
+#endif
+
+#if KHAOS_LANGUAGE_IS(Extensions)
+  CHECK(false);
+#else
+  CHECK(true);
+#endif
+
 }
