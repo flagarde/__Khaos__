c39cd1440cad0e47ed9b60b974c2a3a0bbb1641c
flagarde <flagarde.programs@pm.me>
2021-11-29 19:48:54 +0800

    Add VersionMacros.h, C.h, CXX.h

--- tests/LanguageC.test.c
+++ tests/LanguageC.test.c
@@ -1,8 +1,8 @@
 #include "khaos/Languages.h"
 
 /* Cannot use doctest */
-#if KHAOS_LANGUAGE_IS(c)
+#if KHAOS_LANGUAGE_IS(C)
 
 #else
-  #error "KHAOS_LANGUAGE_IS(c) should be true !"
+  #error "KHAOS_LANGUAGE_IS(C) should be true !"
 #endif
